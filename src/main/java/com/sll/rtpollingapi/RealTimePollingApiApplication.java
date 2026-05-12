d6651b3475802c3944737b8275dab657755dc66e
Souvik-D10 <svkc.cht@gmail.com>
2026-05-12 21:06:32 +0530

    Changes

--- src/main/java/com/sll/rtpollingapi/RealTimePollingApiApplication.java
+++ src/main/java/com/sll/rtpollingapi/RealTimePollingApiApplication.java
@@ -2,10 +2,8 @@ package com.sll.rtpollingapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class RealTimePollingApiApplication {
 
 	public static void main(String[] args) {
